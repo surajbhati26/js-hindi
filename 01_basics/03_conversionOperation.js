96a2c89fa593cd72b857f2afcd84ffd3a5e4333b
SURAJ KANWAR <surajkanwar2005@gmail.com>
2026-09-21 08:04:34 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score = true
+
+//console.log(typeof score);
+// console.log(typeof (score));
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//NaN => not a number
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// "Suraj" => NaN
+let isLoggedIn = "Suraj"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 =>true; 0 => false
+//"" => false; "Suraj" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
+
+
+
+
+
