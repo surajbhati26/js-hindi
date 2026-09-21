f86969c8a3d4775fc5a3a659dab69afe151e27cc
SURAJ KANWAR <surajkanwar2005@gmail.com>
2026-09-21 23:52:12 +0000

    comparisions

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,32 @@
+/* console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2 == 1);
+console.log(2 != 1); */
+
+//console.log("2" > 1);
+//console.log("02" > 1);
+// it gives right value because it automatically converts string into number but it is not not right method so avoid comparing 2 different datatypes
+// in typescript there is rule that two different datatypes should not be compared 
+
+
+
+console.log(null > 0);  //false
+console.log(null == 0);  //false
+console.log(null >= 0);  //true
+
+// reason : equality check == and comparisions <,>,>=,<= work differently.
+// comparision converts null to a number that is 0 thats why above results are obtained.
+console.log(undefined == 0);  //false
+console.log(undefined > 0);  //false
+console.log(undefined < 0);  //false
+// we should avoid such comparisions
+
+
+
+
+// strict check  ===
+
+console.log("2" === 2);  //false because datatypes are different and "2" will not be converted into number and remains string
+
+
